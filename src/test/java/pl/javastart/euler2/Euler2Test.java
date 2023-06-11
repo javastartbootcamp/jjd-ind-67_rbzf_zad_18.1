07ff6d91e284c2829471bd347e3e02d4f71b73f1
rbzf <anna.sadkowska@gmail.com>
2023-06-11 06:22:48 +0200

    Rozwiazanie poprawione

--- src/test/java/pl/javastart/euler2/Euler2Test.java
+++ src/test/java/pl/javastart/euler2/Euler2Test.java
@@ -6,29 +6,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class Euler2Test {
 
     @Test
-    void shouldReturn0For1(){
+    void shouldReturn0For1() {
         int sum = Euler2.calculateSum(1);
         assertEquals(0, sum);
     }
     @Test
-    void shouldReturn2For2(){
+    void shouldReturn2For2() {
         int sum = Euler2.calculateSum(2);
         assertEquals(2, sum);
     }
     @Test
-    void shouldReturn2For7(){
+    void shouldReturn2For7() {
         int sum = Euler2.calculateSum(7);
         assertEquals(2, sum);
     }
 
     @Test
-    void shouldReturn10For10(){
+    void shouldReturn10For10() {
         int sum = Euler2.calculateSum(10);
         assertEquals(10, sum);
     }
 
     @Test
-    void shouldReturn44For34(){
+    void shouldReturn44For34() {
         int sum = Euler2.calculateSum(34);
         assertEquals(44, sum);
     }
